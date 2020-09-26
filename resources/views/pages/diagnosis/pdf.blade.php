c2a67d6d6be6b2496917ca50798af508d570f510
Unknownymous <corgioraynor@live.com.ph>
2020-09-26 10:36:38 +0800

    Update pdf.blade.php
     Add Jquery CDN

--- resources/views/pages/diagnosis/pdf.blade.php
+++ resources/views/pages/diagnosis/pdf.blade.php
@@ -16,9 +16,15 @@
         <img class="img-fluid" src="{{ asset('dist/img/docsHeader/docsHeader.png') }}">
     </div>
 
+    {{-- <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script> --}}
 
 
-    <!-- JS, Popper.js, and jQuery -->
+        <!-- JS, Popper.js, and jQuery -->
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous">
+    </script>
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew     +OrCXaRkfj" crossorigin="anonymous">
     </script>
 
@@ -28,7 +34,8 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
     </script>
 
-    
+
+
 </body>
 
 </html>
