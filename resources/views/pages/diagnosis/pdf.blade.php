922aea6ac0e33159469f9b3424f8cd40d9c7e2e9
Unknownymous <corgioraynor@live.com.ph>
2020-09-28 21:57:04 +0800

    Update pdf.blade.php
    Enhance pdf reporing for paragraphs only

--- resources/views/pages/diagnosis/pdf.blade.php
+++ resources/views/pages/diagnosis/pdf.blade.php
@@ -14,6 +14,98 @@
 <body>
     <div class="container-fluid">
         <img class="img-fluid" src="{{ asset('dist/img/docsHeader/docsHeader.png') }}">
+
+
+                <div class="row">
+            <div class="col"></div>
+            <div class="col align-self-center">
+                <div class="row">
+                    <div class="col d-flex d-xl-flex justify-content-between align-items-center align-content-center align-self-stretch justify-content-xl-start align-items-xl-end">
+                        <div class="form-group text-center d-flex d-xl-flex justify-content-between align-content-center justify-content-xl-center"><label style="font-style: italic;font-weight: bold;">DATE:&nbsp;</label><span>DATE HERE</span></div>
+                    </div>
+                    <div class="col d-flex d-xl-flex justify-content-between align-items-center align-content-center align-self-stretch justify-content-xl-start align-items-xl-end">
+                        <div class="form-group text-center d-flex d-xl-flex justify-content-between align-content-center justify-content-xl-center"><label style="font-style: italic;font-weight: bold;">FILE #:&nbsp;</label><span>DATE HERE</span></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="row">
+                    <div class="col d-flex d-xl-flex justify-content-between align-items-center align-content-center align-self-stretch justify-content-xl-start align-items-xl-end" style="padding-left: 10%;">
+                        <div class="form-group text-center d-flex d-xl-flex justify-content-between align-content-center justify-content-xl-center"><label style="font-style: italic;font-weight: bold;"><span style="text-decoration: underline;">NAME of PATIENT</span>:&nbsp;<span>Name here</span></label></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="row">
+                    <div class="col d-flex d-xl-flex justify-content-between align-items-center align-content-center align-self-stretch justify-content-xl-start align-items-xl-end"></div>
+                    <div class="col d-flex d-xl-flex justify-content-between align-items-center align-content-center align-self-center justify-content-xl-start align-items-xl-end">
+                        <div class="form-group text-center d-flex d-xl-flex justify-content-between align-content-center justify-content-xl-center"><label style="font-style: italic;font-weight: bold;">AGE:&nbsp;</label><span>Age here</span></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col align-self-center">
+                <div class="row">
+                    <div class="col d-flex d-xl-flex justify-content-between align-items-center align-content-center align-self-stretch justify-content-xl-start align-items-xl-end">
+                        <div class="form-group text-center d-flex d-xl-flex justify-content-between align-content-center justify-content-xl-center"><label style="font-style: italic;font-weight: bold;">GENDER:&nbsp;</label><span>Gender HERE</span></div>
+                    </div>
+                    <div class="col d-flex d-xl-flex justify-content-between align-items-center align-content-center align-self-stretch justify-content-xl-start align-items-xl-end">
+                        <div class="form-group text-center d-flex d-xl-flex justify-content-between align-content-center justify-content-xl-center"><label style="font-style: italic;font-weight: bold;">C.S. :&nbsp;</label><span>CSHERE</span></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="row">
+                    <div class="col d-flex d-xl-flex justify-content-between align-items-center align-content-center align-self-stretch justify-content-xl-start align-items-xl-end" style="max-width: 25%;"></div>
+                    <div class="col d-flex d-xl-flex justify-content-between align-items-center align-content-center align-self-center justify-content-xl-start align-items-xl-end">
+                        <div class="form-group text-center d-flex d-xl-flex justify-content-between align-content-center justify-content-xl-center"><label style="font-style: italic;font-weight: bold;">ADDRESS:&nbsp;</label><span>Age here</span></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="row">
+                    <div class="col d-flex d-xl-flex justify-content-between align-items-center align-content-center align-self-stretch justify-content-xl-start align-items-xl-end" style="max-width: 25%;"></div>
+                    <div class="col d-flex d-xl-flex justify-content-between align-items-center align-content-center align-self-center justify-content-xl-start align-items-xl-end">
+                        <div class="form-group text-center d-flex d-xl-flex justify-content-between align-content-center justify-content-xl-center"><label style="font-style: italic;font-weight: bold;">REFFERRING PHYSICIAN:&nbsp;&nbsp;</label><span>Age here</span></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center align-items-center" style="padding-right: 1%;padding-left: 1%;border-top-style: none;">
+            <div class="col d-flex justify-content-center align-items-center align-content-center align-self-center justify-content-xl-center align-items-xl-center" style="/*padding-left: 0;*//*padding-right: 0;*//*padding-bottom: 0;*//*padding-top: 0;*/border-top-style: solid;border-bottom-style: solid;">
+                <div class="form-group text-center d-flex d-xl-flex justify-content-between align-content-center justify-content-xl-center align-items-xl-center"><label class="text-danger" style="font-style: normal;font-weight: bold;">REPORT&nbsp;TITLE HERE..</label></div>
+            </div>
+        </div>
+        <div class="row" style="padding-top: 5%;padding-right: 2%;padding-left: 2%;padding-bottom: 5%;">
+            <div class="col">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non. Nunc congue nisi vitae suscipit tellus mauris.<br><br>Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non. Nunc congue nisi vitae suscipit tellus mauris.<br><br>Lorem ipsum dolor sit amet, consectetur adipiscing
+                    elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non. Nunc congue nisi vitae suscipit tellus mauris.<br><br></p>
+            </div>
+        </div>
+        <div class="row" style="padding-right: 2%;padding-left: 2%;">
+            <div class="col">
+                <h1 style="font-size: 15px;font-weight: bold;">IMPRESSION:</h1>
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                    <li>Item 4</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row justify-content-center align-items-center" style="padding-right: 1%;padding-left: 1%;border-style: none;border-top-style: none;">
+            <div class="col d-flex justify-content-center align-items-center align-content-center align-self-center justify-content-xl-center align-items-xl-center" style="/*padding-left: 0;*//*padding-right: 0;*//*padding-bottom: 0;*//*padding-top: 0;*//*border-top-style: solid;*//*border-bottom-style: solid;*/">
+                <div class="form-group text-center d-flex d-xl-flex flex-column justify-content-between align-content-center justify-content-xl-center align-items-xl-center"><label class="text-primary" style="font-style: normal;font-weight: bold;font-size: 12px;">DOCTOR FULL NAME</label><span style="font-size: 11px;">Doctor details here...</span></div>
+            </div>
+        </div>
     </div>
 
     {{-- <script src="assets/js/jquery.min.js"></script>
@@ -21,7 +113,7 @@
 
 
     <!-- JS, Popper.js, and jQuery -->
-    
+
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
