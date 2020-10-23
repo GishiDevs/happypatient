4401652b740aea02f991e362820c45f63a72930d
Unknownymous <corgioraynor@live.com.ph>
2020-10-23 18:38:35 +0800

    Update pdf.blade.php
    Map laravel data to pdf

--- resources/views/pages/diagnosis/pdf.blade.php
+++ resources/views/pages/diagnosis/pdf.blade.php
@@ -114,98 +114,295 @@
             </table>
         </div> --}}
 
-        <div class="table-responsive" style="border-style: none;">
-    <table class="table table-borderless table-sm">
-        <thead class="invisible" style="border-style: none;">
-            <tr>
-                <th>Column 1</th>
-                <th>Column 1</th>
-                <th>Column 2</th>
-                <th>Column 2</th>
-            </tr>
-        </thead>
-        <tbody style="border-style: none;">
-            <tr style="border-style: none;border-color: rgb(255,255,255);border-top-style: none;">
-                <td style="border-style: none;border-color: rgba(33,37,41,0);height: 10px;"></td>
-                <td style="width: 25%;border-style: none;border-color: rgb(255,255,255);"></td>
-                <td style="width: 25%;border-style: none;border-color: rgb(255,255,255);"><strong style="font-size: 14px;"><em>DATE: </em></strong><span style="font-size: 14px;">Date here</span></td>
-                <td style="width: 25%;border-style: none;"><strong style="font-size: 14px;"><em>FILE #: </em></strong><span style="font-size: 14px;">FILE# here</span></td>
-            </tr>
-            <tr>
-                <td style="font-size: 14px;width: 10%;" colspan="4"><strong><em>NAME OF PATIENT: </em></strong><span style="font-size: 14px;">Name here</span></td>
-            </tr>
-            <tr style="border-style: none;border-color: rgb(255,255,255);border-top-style: none;">
-                <td class="text-right" style="border-style: none;border-color: rgb(255,255,255);"><strong style="font-size: 14px;"><em>AGE: </em></strong><span style="font-size: 14px;">Age here</span></td>
-                <td class="text-center" style="border-style: none;border-color: rgb(255,255,255);"><strong style="font-size: 14px;"><em>GENDER: </em></strong><span style="font-size: 14px;">TEXT here</span></td>
-                <td style="border-style: none;"><strong style="font-size: 14px;"><em>C.S. </em></strong><span style="font-size: 14px;">TEXT HERE</span></td>
-            </tr>
-            <tr style="border-style: none;border-color: rgb(255,255,255);border-top-style: none;">
-                <td class="text-right" style="border-style: none;border-color: rgb(255,255,255);"><strong style="font-size: 14px;"><em>ADDRESS :</em></strong></td>
-                <td class="text-left" style="border-style: none;border-color: rgb(255,255,255);padding: 0;padding-top: 4.8PX;" colspan="3"><span style="font-size: 14px;">TEXT here</span></td>
-            </tr>
-            <tr style="border-style: none;border-color: rgb(255,255,255);border-top-style: none;">
-                <td class="text-right" style="border-style: none;border-color: rgb(255,255,255);"><strong style="font-size: 14px;"><em>REFERRING PHYSICIAN: </em></strong></td>
-                <td class="text-left" style="border-style: none;border-color: rgb(255,255,255);padding: 0;padding-top: 4.8PX;" colspan="3"><span style="font-size: 14px;">TEXT here</span></td>
-            </tr>
-            <tr></tr>
-            <tr>
-                <td class="text-uppercase text-center text-danger" colspan="4" style="height: 25px;"></td>
-            </tr>
-            <tr>
-                <td class="text-uppercase" colspan="4" style="height: 10px;border-width: 1px;border-top-style: solid;"></td>
-            </tr>
-            <tr>
-                <td class="text-uppercase text-center text-danger" style="font-size: 12px;text-align: center;" colspan="4"><strong style="font-size: 16px;">template name</strong></td>
-            </tr>
-            <tr>
-                <td colspan="4" style="height: 10px;border-width: 1px;border-bottom-style: solid;"></td>
-            </tr>
-            <tr>
-                <td colspan="4" style="height: 25px;border-style: none;"></td>
-            </tr>
-            <tr>
-                <td class="text-uppercase text-center text-danger" style="font-size: 12px;text-align: center;color: rgb(0,0,0);border-style: none;border-color: rgb(255,255,255);border-top-width: 1px;border-top-color: rgb(0,0,0);border-bottom-color: rgb(0,0,0);"
-                    colspan="4">
-                    <p style="color: rgb(0,0,0);text-align: left;font-size: 14px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu non odio euismod lacinia at. Non odio euismod lacinia at quis risus sed vulputate odio. Lorem ipsum dolor
-                        sit amet consectetur adipiscing elit ut aliquam. Arcu cursus vitae congue mauris rhoncus. Semper quis lectus nulla at. In arcu cursus euismod quis viverra. Sed nisi lacus sed viverra tellus. Dui faucibus in ornare quam viverra.
-                        Neque ornare aenean euismod elementum nisi quis eleifend.<br /></p>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="4" style="height: 25px;border-style: none;"></td>
-            </tr>
-            <tr>
-                <td class="text-uppercase text-center" style="font-size: 12px;text-align: left;color: rgb(0,0,0);border-style: none;border-color: rgb(255,255,255);border-top-width: 1px;border-top-color: rgb(0,0,0);border-bottom-color: rgb(0,0,0);font-weight: normal;"
-                    colspan="4">
-                    <h1 style="text-align: left;font-size: 14px;font-weight: bold;">IMPRESSION:</h1>
-                </td>
-            </tr>
-            <tr>
-                <td class="text-uppercase text-center" style="font-size: 12px;text-align: left;color: rgb(0,0,0);border-style: none;border-color: rgb(255,255,255);border-top-width: 1px;border-top-color: rgb(0,0,0);border-bottom-color: rgb(0,0,0);font-weight: normal;"
-                    colspan="4">
-                    <ul style="text-align: left;font-size: 14px;">
-                        <li>Item 1</li>
-                        <li>Item 2</li>
-                        <li>Item 3</li>
-                        <li>Item 4</li>
-                    </ul>
-                </td>
-            </tr>
-            <tr>
-                <td class="text-uppercase text-center" style="font-size: 12px;text-align: left;color: rgb(0,0,0);border-style: none;border-color: rgb(255,255,255);border-top-width: 1px;border-top-color: rgb(0,0,0);border-bottom-color: rgb(0,0,0);font-weight: normal;"
-                    colspan="4">
-                    <h1 style="text-align: center;font-size: 10px;font-weight: bold;color: rgb(79,67,222);">DOCTOR FULL NAME</h1>
-                </td>
-            </tr>
-            <tr>
-                <td class="text-uppercase text-center" style="font-size: 12px;text-align: left;color: rgb(0,0,0);border-style: none;border-color: rgb(255,255,255);border-top-width: 1px;border-top-color: rgb(0,0,0);border-bottom-color: rgb(0,0,0);font-weight: normal;"></td>
-                <td class="text-uppercase text-center" style="font-size: 12px;text-align: left;color: rgb(0,0,0);border-style: none;border-color: rgb(255,255,255);border-top-width: 1px;border-top-color: rgb(0,0,0);border-bottom-color: rgb(0,0,0);font-weight: normal;"
-                    colspan="2"><span style="font-size: 8px;color: rgb(51,43,142);">Doctor details</span></td>
-                <td class="text-uppercase text-center" style="font-size: 12px;text-align: left;color: rgb(0,0,0);border-style: none;border-color: rgb(255,255,255);border-top-width: 1px;border-top-color: rgb(0,0,0);border-bottom-color: rgb(0,0,0);font-weight: normal;"></td>
-            </tr>
-        </tbody>
-    </table>
-</div>
+        <div class="table-responsive" style="border-style: none">
+            <table class="table table-borderless table-sm">
+                <thead class="invisible" style="border-style: none">
+                <tr>
+                    <th>Column 1</th>
+                    <th>Column 1</th>
+                    <th>Column 2</th>
+                    <th>Column 2</th>
+                </tr>
+                </thead>
+                <tbody style="border-style: none">
+                <tr
+                    style="
+                    border-style: none;
+                    border-color: rgb(255, 255, 255);
+                    border-top-style: none;
+                    "
+                >
+                    <td
+                    style="
+                        border-style: none;
+                        border-color: rgba(33, 37, 41, 0);
+                        height: 10px;
+                    "
+                    ></td>
+                    <td
+                    style="
+                        width: 25%;
+                        border-style: none;
+                        border-color: rgb(255, 255, 255);
+                    "
+                    ></td>
+                    <td
+                    style="
+                        width: 25%;
+                        border-style: none;
+                        border-color: rgb(255, 255, 255);
+                    "
+                    >
+                    <strong style="font-size: 14px"><em>DATE: </em></strong
+                    ><span style="font-size: 14px">{{ $patient_service->docdate }}</span>
+                    </td>
+                    <td style="width: 25%; border-style: none">
+                    <strong style="font-size: 14px"><em>FILE #: </em></strong
+                    ><span style="font-size: 14px">{{ $patient_service->file_no }}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="font-size: 14px; width: 10%" colspan="4">
+                    <strong><em>NAME OF PATIENT: </em></strong
+                    ><span style="font-size: 14px">{{ $patient_service->name }}</span>
+                    </td>
+                </tr>
+                <tr
+                    style="
+                    border-style: none;
+                    border-color: rgb(255, 255, 255);
+                    border-top-style: none;
+                    "
+                >
+                    <td
+                    class="text-right"
+                    style="border-style: none; border-color: rgb(255, 255, 255)"
+                    >
+                    <strong style="font-size: 14px"><em>AGE: </em></strong
+                    ><span id="age" style="font-size: 14px">Age here</span>
+                    </td>
+                    <td
+                    class="text-center"
+                    style="border-style: none; border-color: rgb(255, 255, 255)"
+                    >
+                    <strong style="font-size: 14px"><em>GENDER: </em></strong
+                    ><span style="font-size: 14px">{{ $patient_service->gender }}</span>
+                    </td>
+                    <td style="border-style: none">
+                    <strong style="font-size: 14px"><em>C.S. </em></strong
+                    ><span style="font-size: 14px"
+                        >{{ $patient_service->civilstatus }}</span
+                    >
+                    </td>
+                </tr>
+                <tr
+                    style="
+                    border-style: none;
+                    border-color: rgb(255, 255, 255);
+                    border-top-style: none;
+                    "
+                >
+                    <td
+                    class="text-right"
+                    style="border-style: none; border-color: rgb(255, 255, 255)"
+                    >
+                    <strong style="font-size: 14px"><em>ADDRESS :</em></strong>
+                    </td>
+                    <td
+                    class="text-left"
+                    style="
+                        border-style: none;
+                        border-color: rgb(255, 255, 255);
+                        padding: 0;
+                        padding-top: 4.8px;
+                    "
+                    colspan="3"
+                    >
+                    <span style="font-size: 14px">{{ $patient_service->address }}</span>
+                    </td>
+                </tr>
+                <tr
+                    style="
+                    border-style: none;
+                    border-color: rgb(255, 255, 255);
+                    border-top-style: none;
+                    "
+                >
+                    <td
+                    class="text-right"
+                    style="border-style: none; border-color: rgb(255, 255, 255)"
+                    >
+                    <strong style="font-size: 14px"
+                        ><em>REFERRING PHYSICIAN: </em></strong
+                    >
+                    </td>
+                    <td
+                    class="text-left"
+                    style="
+                        border-style: none;
+                        border-color: rgb(255, 255, 255);
+                        padding: 0;
+                        padding-top: 4.8px;
+                    "
+                    colspan="3"
+                    >
+                    <span style="font-size: 14px">{{ $patient_service->physician }}</span>
+                    </td>
+                </tr>
+                <tr></tr>
+                <tr>
+                    <td
+                    class="text-uppercase text-center text-danger"
+                    colspan="4"
+                    style="height: 25px"
+                    ></td>
+                </tr>
+                <tr>
+                    <td
+                    class="text-uppercase"
+                    colspan="4"
+                    style="height: 10px; border-width: 1px; border-top-style: solid"
+                    ></td>
+                </tr>
+                <tr>
+                    <td
+                    class="text-uppercase text-center text-danger"
+                    style="font-size: 12px; text-align: center"
+                    colspan="4"
+                    >
+                    <strong style="font-size: 16px">{{ $patient_service->title }}</strong>
+                    </td>
+                </tr>
+                <tr>
+                    <td
+                    colspan="4"
+                    style="height: 10px; border-width: 1px; border-bottom-style: solid"
+                    ></td>
+                </tr>
+                <tr>
+                    <td colspan="4" style="height: 25px; border-style: none"></td>
+                </tr>
+                <tr>
+                    <td
+                    class="text-uppercase text-center text-danger"
+                    style="
+                        font-size: 12px;
+                        text-align: center;
+                        color: rgb(0, 0, 0);
+                        border-style: none;
+                        border-color: rgb(255, 255, 255);
+                        border-top-width: 1px;
+                        border-top-color: rgb(0, 0, 0);
+                        border-bottom-color: rgb(0, 0, 0);
+                    "
+                    colspan="4"
+                    >
+                    <p style="color: rgb(0, 0, 0); text-align: left; font-size: 14px">
+                        {!! $patient_service->content !!}
+                    </p>
+                    </td>
+                </tr>
+                <!-- <tr>
+                            <td colspan="4" style="height: 25px;border-style: none;"></td>
+                        </tr>
+                        <tr>
+                            <td class="text-uppercase text-center" style="font-size: 12px;text-align: left;color: rgb(0,0,0);border-style: none;border-color: rgb(255,255,255);border-top-width: 1px;border-top-color: rgb(0,0,0);border-bottom-color: rgb(0,0,0);font-weight: normal;"
+                                colspan="4">
+                                <h1 style="text-align: left;font-size: 14px;font-weight: bold;">IMPRESSION:</h1>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="text-uppercase text-center" style="font-size: 12px;text-align: left;color: rgb(0,0,0);border-style: none;border-color: rgb(255,255,255);border-top-width: 1px;border-top-color: rgb(0,0,0);border-bottom-color: rgb(0,0,0);font-weight: normal;"
+                                colspan="4">
+                                <ul style="text-align: left;font-size: 14px;">
+                                    <li>Item 1</li>
+                                    <li>Item 2</li>
+                                    <li>Item 3</li>
+                                    <li>Item 4</li>
+                                </ul>
+                            </td>
+                        </tr> -->
+                <tr>
+                    <td
+                    class="text-uppercase text-center"
+                    style="
+                        font-size: 12px;
+                        text-align: left;
+                        color: rgb(0, 0, 0);
+                        border-style: none;
+                        border-color: rgb(255, 255, 255);
+                        border-top-width: 1px;
+                        border-top-color: rgb(0, 0, 0);
+                        border-bottom-color: rgb(0, 0, 0);
+                        font-weight: normal;
+                    "
+                    colspan="4"
+                    >
+                    <h1
+                        style="
+                        text-align: center;
+                        font-size: 10px;
+                        font-weight: bold;
+                        color: rgb(79, 67, 222);
+                        "
+                    >
+                        {{ Auth::user()->name }}
+                    </h1>
+                    </td>
+                </tr>
+                <tr>
+                    <td
+                    class="text-uppercase text-center"
+                    style="
+                        font-size: 12px;
+                        text-align: left;
+                        color: rgb(0, 0, 0);
+                        border-style: none;
+                        border-color: rgb(255, 255, 255);
+                        border-top-width: 1px;
+                        border-top-color: rgb(0, 0, 0);
+                        border-bottom-color: rgb(0, 0, 0);
+                        font-weight: normal;
+                    "
+                    ></td>
+                    <td
+                    class="text-uppercase text-center"
+                    style="
+                        font-size: 12px;
+                        text-align: left;
+                        color: rgb(0, 0, 0);
+                        border-style: none;
+                        border-color: rgb(255, 255, 255);
+                        border-top-width: 1px;
+                        border-top-color: rgb(0, 0, 0);
+                        border-bottom-color: rgb(0, 0, 0);
+                        font-weight: normal;
+                    "
+                    colspan="2"
+                    >
+                    <span style="font-size: 8px; color: rgb(51, 43, 142)"
+                        >Doctor details</span
+                    >
+                    </td>
+                    <td
+                    class="text-uppercase text-center"
+                    style="
+                        font-size: 12px;
+                        text-align: left;
+                        color: rgb(0, 0, 0);
+                        border-style: none;
+                        border-color: rgb(255, 255, 255);
+                        border-top-width: 1px;
+                        border-top-color: rgb(0, 0, 0);
+                        border-bottom-color: rgb(0, 0, 0);
+                        font-weight: normal;
+                    "
+                    ></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+
 
 
     </div>
