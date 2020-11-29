e05824ab2fc24f4ddf7af11fe5c79b3954dd7e43
Unknownymous <corgioraynor@live.com.ph>
2020-11-29 22:16:06 +0800

    Update pdf.blade.php

--- resources/views/pages/diagnosis/pdf.blade.php
+++ resources/views/pages/diagnosis/pdf.blade.php
@@ -13,6 +13,14 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js"></script>
 
     <style>
+        * {
+            -webkit-print-color-adjust: exact !important;   /* Chrome, Safari */
+            color-adjust: exact !important;                 /*Firefox*/
+        }
+        .bg-darkk {
+            background-color: lightgrey;
+        }
+
         figure table {
         border-collapse: collapse !important;
         }
@@ -34,10 +42,42 @@
             <div class="d-flex flex-column align-items-xl-start mt-3 ml-3">
                 <div><img src="{{ asset('dist/img/docsHeader/hpdc_description.png') }}" /></div>
                 <div class="mt-2"><img src="{{ asset('dist/img/docsHeader/hpdc_address.png') }}" /></div>
-                <div class="d-flex justify-content-sm-center align-items-sm-center w-100 mt-3 bg-light text-primary text-uppercase"
+                <div class="d-flex justify-content-sm-center align-items-sm-center w-100 mt-3 bg-darkk text-primary text-uppercase"
                     style="height: 50px;">
                     <h2>{{ $patient_service->service }} Report</h2>
                 </div>
+            </div>
+        </div>
+
+        <div class="text-uppercase font-weight-bold font-italic" style="font-size: 17px;">
+            <div class="container">
+                <div class="d-flex d-sm-flex flex-row justify-content-sm-end">
+                    <p class="my-0 mr-5">Date: {{ $patient_service->docdate }}</p>
+                    <p class="my-0">File#: {{ $patient_service->file_no }}</p>
+                </div>
+                <div class="d-flex flex-row justify-content-lg-start align-items-lg-center">
+                    <p class="my-0">Name of patient: {{ $patient_service->name }}</p>
+                </div>
+                <div class="d-flex flex-column justify-content-lg-start align-items-lg-start pl-5 ml-5 mt-3">
+                    <div class="d-flex justify-content-between" style="width: 400px;">
+                        <p class="my-0">Age:
+                            <span style="font-size: 18px" id="age"></span>
+                        </p>
+                        <p class="my-0">Gender: {{ $patient_service->gender }}</p>
+                        <p class="my-0">C.S.: {{ $patient_service->civilstatus }}</p>
+                    </div>
+                    <p class="my-0">Address: {{ $patient_service->address . ' ' . $patient_service->location}}</p>
+                    <p class="my-0">referring physician: {{ $patient_service->physician }}</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="container mt-2">
+            <div class="text-center border-top border-bottom border-dark py-2">
+                <h4 class="text-break text-uppercase text-info my-0">{{ $patient_service->title }}</h4>
+            </div>
+            <div style="font-size: 16px;">
+                {!! $patient_service->content !!}
             </div>
         </div>
 
@@ -138,7 +178,7 @@
             </table>
         </div> --}}
 
-        <div class="table-responsive" style="border-style: none">
+        {{-- <div class="table-responsive" style="border-style: none">
             <table class="table table-borderless table-sm">
                 <thead class="invisible" style="border-style: none">
                 <tr>
@@ -307,12 +347,7 @@
                     <td colspan="4" style="height: 25px; border-style: none"></td>
                 </tr>
                 <tr>
-                    <!-- <td class="text-uppercase text-center text-danger" style="font-size: 12px;text-align: center;color: rgb(0,0,0);border-style: none;border-color: rgb(255,255,255);border-top-width: 1px;border-top-color: rgb(0,0,0);border-bottom-color: rgb(0,0,0);"
-                                        colspan="4">
-                                        <p style="color: rgb(0,0,0);text-align: left;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu non odio euismod lacinia at. Non odio euismod lacinia at quis risus sed vulputate odio. Lorem
-                                            ipsum dolor sit amet consectetur adipiscing elit ut aliquam. Arcu cursus vitae congue mauris rhoncus. Semper quis lectus nulla at. In arcu cursus euismod quis viverra. Sed nisi lacus sed viverra tellus. Dui faucibus in
-                                            ornare quam viverra. Neque ornare aenean euismod elementum nisi quis eleifend.<br></p>
-                                    </td> -->
+
                     <td colspan="4">
                     <p style="color: rgb(0, 0, 0); text-align: left">
                         {!! $patient_service->content !!}
@@ -322,107 +357,10 @@
                 <tr>
                     <td colspan="4" style="height: 25px; border-style: none"></td>
                 </tr>
-                <!-- <tr>
-                                    <td class="text-uppercase text-center" style="font-size: 12px;text-align: left;color: rgb(0,0,0);border-style: none;border-color: rgb(255,255,255);border-top-width: 1px;border-top-color: rgb(0,0,0);border-bottom-color: rgb(0,0,0);font-weight: normal;"
-                                        colspan="4">
-                                        <h1 style="text-align: left;font-size: 12px;font-weight: bold;">IMPRESSION:</h1>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="text-uppercase text-center" style="font-size: 12px;text-align: left;color: rgb(0,0,0);border-style: none;border-color: rgb(255,255,255);border-top-width: 1px;border-top-color: rgb(0,0,0);border-bottom-color: rgb(0,0,0);font-weight: normal;"
-                                        colspan="4">
-                                        <ul style="text-align: left;">
-                                            <li>Item 1</li>
-                                            <li>Item 2</li>
-                                            <li>Item 3</li>
-                                            <li>Item 4</li>
-                                        </ul>
-                                    </td>
-                                </tr> -->
-                {{-- <tr>
-                    <td
-                    class="text-uppercase text-center"
-                    style="
-                        font-size: 12px;
-                        text-align: left;
-                        color: rgb(0, 0, 0);
-                        border-style: none;
-                        border-color: rgb(255, 255, 255);
-                        border-top-width: 1px;
-                        border-top-color: rgb(0, 0, 0);
-                        border-bottom-color: rgb(0, 0, 0);
-                        font-weight: normal;
-                    "
-                    colspan="4"
-                    >
-                    <h1
-                        style="
-                        text-align: center;
-                        font-size: 10px;
-                        font-weight: bold;
-                        color: rgb(79, 67, 222);
-                        "
-                    >
-                    {{ Auth::user()->name }}
-                    </h1>
-                    </td>
-                </tr> --}}
-                {{-- <tr>
-                    <td
-                    class="text-uppercase text-center"
-                    style="
-                        font-size: 12px;
-                        text-align: left;
-                        color: rgb(0, 0, 0);
-                        border-style: none;
-                        border-color: rgb(255, 255, 255);
-                        border-top-width: 1px;
-                        border-top-color: rgb(0, 0, 0);
-                        border-bottom-color: rgb(0, 0, 0);
-                        font-weight: normal;
-                    "
-                    ></td>
-                    <td
-                    class="text-uppercase text-center"
-                    style="
-                        font-size: 12px;
-                        text-align: left;
-                        color: rgb(0, 0, 0);
-                        border-style: none;
-                        border-color: rgb(255, 255, 255);
-                        border-top-width: 1px;
-                        border-top-color: rgb(0, 0, 0);
-                        border-bottom-color: rgb(0, 0, 0);
-                        font-weight: normal;
-                    "
-                    colspan="2"
-                    >
-                    <span style="font-size: 8px; color: rgb(51, 43, 142)"
-                        >{{ Auth::user()->description }}</span
-                    >
-                    <br>
-                    <span style="font-size: 8px; color: rgb(51, 43, 142)"
-                        >{{ Auth::user()->license }}</span
-                    >
-                    </td>
-                    <td
-                    class="text-uppercase text-center"
-                    style="
-                        font-size: 12px;
-                        text-align: left;
-                        color: rgb(0, 0, 0);
-                        border-style: none;
-                        border-color: rgb(255, 255, 255);
-                        border-top-width: 1px;
-                        border-top-color: rgb(0, 0, 0);
-                        border-bottom-color: rgb(0, 0, 0);
-                        font-weight: normal;
-                    "
-                    ></td>
-                </tr> --}}
+
                 </tbody>
             </table>
-        </div>
+        </div> --}}
 
 
         <div class="d-flex flex-column justify-content-center align-items-center">
@@ -439,10 +377,6 @@
 
 
     </div>
-
-    {{-- <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script> --}}
-
 
     <!-- JS, Popper.js, and jQuery -->
 
