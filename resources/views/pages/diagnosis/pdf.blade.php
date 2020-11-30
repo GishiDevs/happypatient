d497cdeaa4346d7d17a365ef77e1ba671f563191
Unknownymous <corgioraynor@live.com.ph>
2020-11-30 11:30:44 +0800

    Update pdf.blade.php

--- resources/views/pages/diagnosis/pdf.blade.php
+++ resources/views/pages/diagnosis/pdf.blade.php
@@ -366,7 +366,7 @@
             <div class="{{ $patient_service->service === "Laboratory" ? "d-sm-block" : "d-sm-none" }} d-flex flex-column justify-content-center align-items-center">
                 <div class="d-flex flex-column justify-content-center align-items-center"><img class="img-fluid" src="{{ asset('dist/img/docsHeader/geraldine.png') }}" /></div>
                 <div class="text-nowrap">
-                    <h6 class="my-0" style="color: #7030a0;font-size: 15px;font-style: normal;font-weight: bold;">GERALDINE M. AGPES, MD, FPSP</h6>
+                    <h6 class="text-uppercase my-0" style="color: #7030a0;font-size: 15px;font-style: normal;font-weight: bold;">GERALDINE M. AGPES, MD, FPSP</h6>
                 </div>
                 <div class="text-break text-center" style="width: 250px;color: #7030a0;">
                     <p class="text-center my-0" style="font-size: 12px;">Pathologist</p>
@@ -375,7 +375,7 @@
             </div>
             <div class="d-flex flex-column justify-content-center align-items-center">
                 <div class="text-nowrap">
-                    <h6 class="my-0" style="color: #7030a0;font-size: 15px;font-style: normal;font-weight: bold;">{{ Auth::user()->name }}</h6>
+                    <h6 class="text-uppercase my-0" style="color: #7030a0;font-size: 15px;font-style: normal;font-weight: bold;">{{ Auth::user()->name }}</h6>
                 </div>
                 <div class="text-break text-center" style="width: 250px;color: #7030a0;">
                     <p class="text-center my-0" style="font-size: 12px;">{{ Auth::user()->description }}</p>
